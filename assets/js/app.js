5ecd87d8a8cbe325270c4e89158aa6267ed2ac1b
Ximo Romero <ximore55@gmail.com>
2023-08-15 19:58:15 +1200

    added fetch js

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+(function () {
+  const container = document.querySelector(".summary_list");
+
+  fetch("../../data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((item) => {
+        let newItem = document.createElement("li");
+        newItem.innerHTML = `
+        <img
+        src="${item.icon}"
+        alt=""
+        class="icon__summary"
+      />
+      <p class="title__summary">${item.category}</p>
+      <div class="puntuation_summary">
+        <p><small>${item.score}</small><small> / 100</small></p>
+      </div>
+        `;
+        newItem.classList.add("summary_item");
+
+        container.append(newItem);
+      });
+    });
+})();
